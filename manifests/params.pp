3b444f8c5eda8813c9235ea19ee7d6e41392cd2e
James Allenby <James.Allenby@immediate.co.uk>
2024-02-09 16:03:22 +0000

    fix: remove legacy fact

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,20 +1,20 @@
 # A class for parameters we might need to use.
 class wp::params {
-	$user = $::operatingsystem ? {
+	$user = $facts['os']['name'] ? {
 		'windows' => undef,
 		default   => 'www-data',
 	}
 	$bin_path = '/usr/local/bin'
-	$executable_filename = $::operatingsystem ? {
+	$executable_filename = $facts['os']['name'] ? {
 		'windows' => 'wp.bat',
 		default   => 'wp',
 	}
-	$php_package = $::operatingsystem ? {
+	$php_package = $facts['os']['name'] ? {
 		/^(Debian|Ubuntu)$/ => 'php5-cli',
 		'windows'           => 'php',
 		default             => 'php-cli',
 	}
-	$php_executable_path = $::operatingsystem ? {
+	$php_executable_path = $facts['os']['name'] ? {
 		'windows' => 'C:/tools/php80/php.exe',
 		default   => '/usr/bin/php',
 	}
